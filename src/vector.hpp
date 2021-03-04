ef01d5d4f0493754b6f702385de4c90ea28994d6
Alex Studer <alex@studer.dev>
2021-03-03 23:38:56 -0500

    define Vector3i struct

---
+++ src/vector.hpp
@@ -0,0 +1,16 @@
+#ifndef TIGAME_VECTOR_HPP
+#define TIGAME_VECTOR_HPP
+
+#include <cstdint>
+
+namespace tigame
+{
+	struct Vector3i
+	{
+		uint32_t x;
+		uint32_t y;
+		uint32_t z;
+	};
+}
+
+#endif
