725e94da6878de42c9acd979fe20561af89b4138
Alex Studer <alex@studer.dev>
2021-06-01 23:06:47 -0400

    editor: update OrbitComponent class name

--- editor/main.cpp
+++ editor/main.cpp
@@ -140,7 +140,7 @@ int main(int argc, char * argv[])
 	tigame::Camera camera = tigame::Camera("Editor Camera", 800, 600);
 	camera.SetPosition(8, 4, 8);
 	camera.LookAt(glm::vec3(0, 0, 0), glm::vec3(0, 1, 0));
-	camera.AddComponent(std::make_shared<tigame::OrbitComponent>());
+	camera.AddComponent(std::make_shared<tigame::OrbitControlsComponent>());
 	scene.AddObject(&camera);
 	scene.SetMainCamera(&camera);
 
